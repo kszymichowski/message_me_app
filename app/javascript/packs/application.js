23a41d200f704b142067b681e6b4028f6ed13e1d
Konstanty Szymichowski <kostek.szymi@gmail.com>
2021-11-24 07:18:38 +0100

    add flash message styling in layouts/messages partial

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,14 @@ import 'jquery'
  
 $(document).on('turbolinks:load', function() {
 $('.ui.dropdown').dropdown();
+$('.message .close')
+  .on('click', function() {
+    $(this)
+      .closest('.message')
+      .transition('fade')
+    ;
+  })
+;
 })
  
 Rails.start()
