43677532195f9f67cd419200da6e970faba696de
Konstanty Szymichowski <kostek.szymi@gmail.com>
2021-11-25 12:36:03 +0100

    add scroll bar to message display div

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,16 +10,23 @@ import "channels"
 import '@doabit/semantic-ui-sass'
 import 'jquery'
  
+window.scroll_bottom = function(){
+  if ($("#messages").length > 0) {
+    $("#messages").scrollTop($("#messages")[0].scrollHeight);
+  }
+}
+
+
+
 $(document).on('turbolinks:load', function() {
 $('.ui.dropdown').dropdown();
 $('.message .close')
   .on('click', function() {
     $(this)
       .closest('.message')
-      .transition('fade')
-    ;
-  })
-;
+      .transition('fade');
+  });
+scroll_bottom();
 })
  
 Rails.start()
